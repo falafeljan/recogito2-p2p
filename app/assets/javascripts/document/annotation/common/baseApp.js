7e89b1f676fd95c70c45dd3724262ea4bb0e63b1
Jan Kaßel <jan@kassel.works>
2019-11-06 15:09:09 +0100

    add experimental p2p annotations
    still contains debug messages, is buggy

--- app/assets/javascripts/document/annotation/common/baseApp.js
+++ app/assets/javascripts/document/annotation/common/baseApp.js
@@ -2,25 +2,69 @@
 define([
   'common/api',
   'common/config',
-  'document/annotation/common/page/header'
-], function(API, Config, Header) {
-
+  'common/utils/placeUtils',
+  'document/annotation/common/page/header',
+  'from-me-to-you',
+], function(API, Config, PlaceUtils, Header, FromMeToYou) {
   var BaseApp = function(annotations, highlighter, selector) {
     this.annotations = annotations;
     this.highlighter = highlighter;
     this.selector = selector;
     this.header = new Header();
+
+    this.notebook = new FromMeToYou.RequestSwarm(
+      'hypermerge:/FHLQ1NF8HE8YdhdSEVjyUGeUcRk9SEshvac3M3jQRvZU',
+      {
+        handleError: function(err) {
+          console.error('error', JSON.stringify(err, null, 2));
+        },
+      }
+    );
+  };
+
+  BaseApp.prototype.loadAnnotations = function(loadIndicator, callback) {
+    var self = this;
+    var callbackWrapper = function(promise) {
+      if (typeof callback !== 'function') {
+        return promise;
+      }
+
+      var ret = callback(promise);
+      return ret instanceof Promise ? ret : promise;
+    };
+
+    return PlaceUtils.initGazetteers().done(function() {
+      var promise2 = API.listAnnotationsInPart(
+        Config.documentId,
+        Config.partSequenceNo
+      );
+      promise2.then(annotations =>
+        console.log('FROM API', JSON.stringify(annotations, null, 2))
+      );
+
+      var promise = new Promise(resolve => {
+        self.notebook.on('ready', () => {
+          self.notebook.getAnnotations().then(resolve);
+        });
+      });
+      callbackWrapper(promise)
+        .then(self.onAnnotationsLoaded.bind(self))
+        .then(loadIndicator.destroy)
+        .catch(self.onAnnotationsLoadError.bind(self))
+        .then(loadIndicator.destroy);
+    });
   };
 
   BaseApp.prototype.onAnnotationsLoaded = function(annotations) {
-    var urlHash = (window.location.hash) ? window.location.hash.substring(1) : false,
-        preselected,
-
-        scrollIntoView = function(bounds) {
-          var scrollTo = bounds.top - jQuery(window).height() + 100;
-          if (scrollTo > 0)
-            jQuery('html, body').animate({ scrollTop: scrollTo });
-        };
+    console.log(annotations);
+    var urlHash = window.location.hash
+        ? window.location.hash.substring(1)
+        : false,
+      preselected,
+      scrollIntoView = function(bounds) {
+        var scrollTo = bounds.top - jQuery(window).height() + 100;
+        if (scrollTo > 0) jQuery('html, body').animate({ scrollTop: scrollTo });
+      };
 
     this.annotations.add(annotations);
     this.header.incrementAnnotationCount(annotations.length);
@@ -47,53 +91,59 @@ define([
   BaseApp.prototype.upsertAnnotation = function(annotationStub) {
     var self = this;
 
+    const mutate =
+      typeof annotationStub.id !== 'undefined'
+        ? this.notebook.updateAnnotation
+        : this.notebook.createAnnotation;
     self.header.showStatusSaving();
-    API.storeAnnotation(annotationStub)
-       .done(function(annotation) {
-         self.annotations.addOrReplace(annotation);
-         self.header.incrementAnnotationCount();
-         self.header.updateContributorInfo(Config.me);
-         self.header.showStatusSaved();
 
-         // Merge server-provided properties (id, timestamps, etc.) into the annotation
-         jQuery.extend(annotationStub, annotation);
-         self.highlighter.refreshAnnotation(annotationStub);
-       })
-       .fail(function(error) {
-         self.header.showSaveError(error);
-       });
+    mutate(annotationStub)
+      .then(function(annotation) {
+        console.log(JSON.stringify(annotationStub));
+        console.log(JSON.stringify(annotation));
+        self.annotations.addOrReplace(annotation);
+        self.header.incrementAnnotationCount();
+        self.header.updateContributorInfo(Config.me);
+        self.header.showStatusSaved();
+
+        // Merge server-provided properties (id, timestamps, etc.) into the annotation
+        jQuery.extend(annotationStub, annotation);
+        self.highlighter.refreshAnnotation(annotationStub);
+      })
+      .catch(function(error) {
+        self.header.showSaveError(error);
+      });
   };
 
   BaseApp.prototype.upsertAnnotationBatch = function(annotationStubs) {
     var self = this,
-
-        // Finds the original stub that corresponds to the annotation
-        findStub = function(annotation) {
-          return annotationStubs.find(function(stub) {
-            // Determine identity based on the anchor
-            return stub.anchor === annotation.anchor;
-          });
-        };
+      // Finds the original stub that corresponds to the annotation
+      findStub = function(annotation) {
+        return annotationStubs.find(function(stub) {
+          // Determine identity based on the anchor
+          return stub.anchor === annotation.anchor;
+        });
+      };
 
     self.header.showStatusSaving();
     API.storeAnnotationBatch(annotationStubs)
-       .done(function(annotations) {
-         self.annotations.addOrReplace(annotations);
-         self.header.incrementAnnotationCount(annotations.length);
-         self.header.updateContributorInfo(Config.me);
-         self.header.showStatusSaved();
+      .done(function(annotations) {
+        self.annotations.addOrReplace(annotations);
+        self.header.incrementAnnotationCount(annotations.length);
+        self.header.updateContributorInfo(Config.me);
+        self.header.showStatusSaved();
 
-         annotations.forEach(function(annotation) {
-           // Note: it *should* be safe to assume that the annotations come in the same
-           // order as the original stubs, but we'll be a little defensive here, just in case
-           var stub = findStub(annotation);
-           jQuery.extend(stub, annotation);
-           self.highlighter.refreshAnnotation(stub);
-         });
-       })
-       .fail(function(error) {
-         self.header.showSaveError();
-       });
+        annotations.forEach(function(annotation) {
+          // Note: it *should* be safe to assume that the annotations come in the same
+          // order as the original stubs, but we'll be a little defensive here, just in case
+          var stub = findStub(annotation);
+          jQuery.extend(stub, annotation);
+          self.highlighter.refreshAnnotation(stub);
+        });
+      })
+      .fail(function(error) {
+        self.header.showSaveError();
+      });
   };
 
   BaseApp.prototype.onCreateAnnotation = function(selection) {
@@ -104,11 +154,11 @@ define([
 
   BaseApp.prototype.onCreateAnnotationBatch = function(selections) {
     var self = this,
-        annotationStubs = selections.map(function(selection) {
-          if (selection.isNew)
-            self.highlighter.convertSelectionToAnnotation(selection);
-          return selection.annotation;
-        });
+      annotationStubs = selections.map(function(selection) {
+        if (selection.isNew)
+          self.highlighter.convertSelectionToAnnotation(selection);
+        return selection.annotation;
+      });
     self.upsertAnnotationBatch(annotationStubs);
   };
 
@@ -123,36 +173,35 @@ define([
 
     this.highlighter.removeAnnotation(annotation);
     API.deleteAnnotation(annotation.annotation_id)
-       .done(function() {
-         self.annotations.remove(annotation);
-         self.header.incrementAnnotationCount(-1);
-         self.header.showStatusSaved();
-       })
-       .fail(function(error) {
-         self.header.showSaveError(error);
-       });
+      .done(function() {
+        self.annotations.remove(annotation);
+        self.header.incrementAnnotationCount(-1);
+        self.header.showStatusSaved();
+      })
+      .fail(function(error) {
+        self.header.showSaveError(error);
+      });
   };
 
   BaseApp.prototype.onDeleteAnnotationBatch = function(annotations) {
     var self = this,
-        ids = annotations.map(function(a) {
-          return a.annotation_id;
-        });
+      ids = annotations.map(function(a) {
+        return a.annotation_id;
+      });
 
     self.header.showStatusSaving();
     API.deleteAnnotationBatch(ids)
-       .done(function() {
-         self.highlighter.removeAnnotations(annotations);
-         self.annotations.remove(annotations);
-         self.header.incrementAnnotationCount(-annotations.length);
-         self.header.updateContributorInfo(Config.me);
-         self.header.showStatusSaved();
-       })
-       .fail(function(error) {
-         self.header.showSaveError();
-       });
+      .done(function() {
+        self.highlighter.removeAnnotations(annotations);
+        self.annotations.remove(annotations);
+        self.header.incrementAnnotationCount(-annotations.length);
+        self.header.updateContributorInfo(Config.me);
+        self.header.showStatusSaved();
+      })
+      .fail(function(error) {
+        self.header.showSaveError();
+      });
   };
 
   return BaseApp;
-
 });
